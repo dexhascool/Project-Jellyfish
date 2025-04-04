6e3b57fbcfd4132774b4532fe23e6b259d4795d8
DEX <daxtonf11@gmail.com>
2025-04-04 12:28:12 -0600

    Update supported.lua

--- supported.lua
+++ supported.lua
@@ -1,6 +1,6 @@
 return {
-    { name = "Build a Boat for Treasure", github = "https://raw.githubusercontent.com/thelonious-jaha/Project-Lono/main/games/BABFT.lua", placeId = 537413528 },
-    { name = "Legends of Speed", github = "https://raw.githubusercontent.com/thelonious-jaha/Project-Lono/main/games/LoS.lua", placeId = 3101667897 },
-    { name = "Zombie Attack", github = "https://raw.githubusercontent.com/thelonious-jaha/Project-Lono/main/games/ZombieAttack.lua", placeId = 1240123653 },
-    { name = "Natural Disaster Survival", github = "https://raw.githubusercontent.com/thelonious-jaha/Project-Lono/main/games/NDS.lua", placeId = 189707 }
+    { name = "Build a Boat for Treasure", github = "https://raw.githubusercontent.com/dexhascool/Project-Jellyfish/main/games/BABFT.lua", placeId = 537413528 },
+    { name = "Legends of Speed", github = "https://raw.githubusercontent.com/dexhascool/Project-Jellyfish/main/games/LoS.lua", placeId = 3101667897 },
+    { name = "Zombie Attack", github = "https://raw.githubusercontent.com/dexhascool/Project-Jellyfish/main/games/ZombieAttack.lua", placeId = 1240123653 },
+    { name = "Natural Disaster Survival", github = "https://raw.githubusercontent.com/dexhascool/Project-Jellyfish/main/games/NDS.lua", placeId = 189707 }
 }
